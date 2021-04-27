06ffd24448cc3aad51900c09bedebcbcff4dc42b
cabreramo <jose.cabrera200022@gmail.com>
2021-04-26 22:43:44 -0500

    actualizado

--- Parcial2/Login/script.js
+++ Parcial2/Login/script.js
@@ -1,10 +1,30 @@
-function validar(){
- var usuario = document.getElementById("usuario").value;
- var contrasena = document.getElementById("contrasena").value;
+// function validar(){
+//  var usuario = document.getElementById("usuario").value;
+//  var contrasena = document.getElementById("contrasena").value;
 
- if(usuario="admin" && contrasena=="1234"){
-    alert("LOGEADO");
- }else{
-     alert("error");
- }
-}
+//  if(usuario="admin" && contrasena=="1234"){
+//     alert("LOGEADO");
+//  }else{
+//      alert("error");
+//  }
+// }
+
+
+// <!-- JQUERY -->
+ $(document).ready(function()
+ {
+                    $("#validar").click(function(){
+                        var usuario=$('#usuario').val();
+                        var contrasena=$('#contrasena').val();
+
+                     if( usuario='admin' && contrasena=='1234') {
+                        $('#myModal').modal('show');                                         
+                       }
+                     else {
+                        $('.modal-body').html("CONTRASEÑA INCORRECTA");
+                        $('#myModal').modal('show');
+                                         }
+              
+             });
+     
+         });
